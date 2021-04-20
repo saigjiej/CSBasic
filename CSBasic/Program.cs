53265837fb775504a1f4d00c9c0000c6f95bac9e
SukMin Lee <s2019s16@e-mirim.hs.kr>
2021-04-20 12:03:11 +0900

    #2 Console.WriteLine()을 써서 출력함

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -35,8 +35,11 @@ namespace CSBasic
              *  comment
              */
 
-
-            Console.WriteLine("Hello");
+            // 출력
+            Console.Write("이건 안개행");
+            Console.Write(" 입니당\n");
+            Console.WriteLine("");
+            Console.WriteLine("이건 개행");
         }
     }
 }
