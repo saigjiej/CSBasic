aac4dce47717fba58e3107892ce94d6d9fc99558
SukMin Lee <s2019s16@e-mirim.hs.kr>
2021-04-23 16:06:17 +0900

    #9 입력 예시

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -195,6 +195,17 @@ namespace CSBasic
             Console.WriteLine(somea.GetType());
             Console.WriteLine(someb.GetType());
             Console.WriteLine(somec.GetType());
+
+            string input;
+            while (true)
+            {
+                Console.Write("입력>");
+                input = Console.ReadLine();
+                if (input.Equals("q"))
+                    break;
+                else
+                    Console.WriteLine(input);
+            }
         }
     }
 }
