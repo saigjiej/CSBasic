323fd74c6f4827d7912d073cbc3064f4cdd93934
SukMin Lee <s2019s16@e-mirim.hs.kr>
2021-05-06 09:58:22 +0900

    #17 반복문과 배열

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -6,7 +6,12 @@ namespace CSBasic3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] intArray = new int[100];
+            Console.WriteLine(intArray.Length);
+            Console.WriteLine(intArray[0]);
+            Console.WriteLine(intArray[99]);
+            //Console.WriteLine(intArray[100]); // 범위 초과
+            //Console.WriteLine(intArray[-1]);  // 음수 사용 불가
         }
     }
 }
