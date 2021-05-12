61a815fcc2a1f8b6d34a8f1033aaafeba5956bcc
SukMin Lee <s2019s16@e-mirim.hs.kr>
2021-05-13 08:48:24 +0900

    #19 역for문 실습

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -32,6 +32,17 @@ namespace CSBasic3
             {
                 Console.Write((char)j);
             }
+
+            int[] intArray3 = { 1, 2, 3, 4, 5, 6 };
+            for(int k = intArray.Length - 1; k >= 0; k--)
+            {
+                Console.WriteLine(intArray3[k]);
+            }
+            
+            for(int k = intArray3.Length; k > 0; k--)
+            {
+                Console.WriteLine(intArray3[k - 1]);
+            }
         }
     }
 }
