4df46a16b22ccd455e1d602160f322cd8af02657
Lee Sukmin <s2019s16@e-mirim.hs.kr>
2021-07-01 09:47:06 +0900

    #26 메서드 오버로딩 예제

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -31,6 +31,24 @@ namespace CSBasic5
                 */
             }
 
+            public static double Abs(double input)
+            {
+                return (input < 0) ? -input : input;
+            }
+            
+            // Abs ( long )
+            public static long Abs(long input)
+            {
+                return (input < 0) ? -input : input;
+            }
+
+            // Abs ( long )
+            /* Method Signature가 같은 경우 오버로딩 불가!
+            public static int Abs(long a2)
+            {
+                return 0;
+            }
+            */
         }
 
         class Test
@@ -88,6 +106,11 @@ namespace CSBasic5
             Program p = new Program();
             Console.WriteLine(p.instanceVariable);
             p.instanceMethod();
+
+            Console.WriteLine(MyMath.Abs(52));
+            Console.WriteLine(MyMath.Abs(-274));
+            Console.WriteLine(MyMath.Abs(52.273));
+            Console.WriteLine(MyMath.Abs(21474812312323));
         }
     }
 }
