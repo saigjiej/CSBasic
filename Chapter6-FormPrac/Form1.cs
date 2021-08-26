b3241cc2ba32ef3fe32060f89a5b4e6a00e65321
Lee Sukmin <s2019s16@e-mirim.hs.kr>
2021-08-26 09:51:26 +0900

    MessageBox 실습

--- Chapter6-FormPrac/Form1.cs
+++ Chapter6-FormPrac/Form1.cs
@@ -16,5 +16,34 @@ namespace Chapter6_FormPrac
         {
             InitializeComponent();
         }
+
+        private void btnMessageBox1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = DialogResult.None;
+
+            if(sender == btnMessageBox1)
+            {
+                result = MessageBox.Show("안녕하세요?");
+            } 
+            else if (sender == btnMessageBox2)
+            {
+                result = MessageBox.Show("안녕하세요?", "환영인사");
+            }
+            else if (sender == btnMessageBox3)
+            {
+                result = MessageBox.Show("안녕하세요?", "환영인사", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            }
+
+            switch (result)
+            {
+                case DialogResult.OK:
+                case DialogResult.Yes:
+                    MessageBox.Show("좋은 하루~!", "시스템");
+                    break;
+                case DialogResult.No:
+                    MessageBox.Show("가서 쉬세요!", "시스템");
+                    break;
+            }
+        }
     }
 }
