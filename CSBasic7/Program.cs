662fb1e6aca37602021b2b57c46cc9126543df25
Lee Sukmin <s2019s16@e-mirim.hs.kr>
2021-08-31 11:12:19 +0900

    out 키워드 예제

--- CSBasic7/Program.cs
+++ CSBasic7/Program.cs
@@ -25,18 +25,26 @@ namespace CSBasic7
         }
     }
 
-    class Wanted<T>
-        //where T : Student
-    {
-        public T Value;
-        public Wanted(T value)
-        {
-            this.Value = value;
-        }
-    }
+    
 
     class Program
     {
+        static void NextPostion(int x, int y, int vx, int vy,
+            out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y + vy;
+        }
+
+        class Wanted<T>
+        //where T : Student
+        {
+            public T Value;
+            public Wanted(T value)
+            {
+                this.Value = value;
+            }
+        }
 
         class Student
         {
@@ -48,6 +56,26 @@ namespace CSBasic7
 
         static void Main(string[] args)
         {
+            int x = 0;
+            int y = 0;
+            int vx = 1;
+            int vy = 1;
+            Console.WriteLine("현재좌표:" + x + "," + y);
+            NextPostion(x, y, vx, vy, out x, out y);
+
+            Console.WriteLine("숫자 입력:");
+            int output;
+            bool result = int.TryParse(Console.ReadLine(), out output);
+            if (result)
+            {
+                Console.WriteLine("변환성공:" + output);
+            }
+            else
+            {
+                Console.WriteLine("변환실패:" + output);
+            }
+
+
             Products ps = new Products();
             Random rs = new Random();
             int i = rs.Next(0, 6);
